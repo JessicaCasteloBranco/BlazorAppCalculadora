1dfcf195780f4854300a3c192b36c001ea5581c2
Jéssica Castelo Branco <jessicacastelobranco1914@gmail.com>
2023-06-27 11:16:40 -0400

    Adicionar arquivos de projeto.

---
+++ Pages/Imc.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BlazorAppCalculadora.Pages
+{
+    public class ImcModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
